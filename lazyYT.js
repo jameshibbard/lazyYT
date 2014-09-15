2581a7f1ceb0c803a3447f52d8e0ba48165ee409
James Hibbard <info@hibbard.eu>
2014-09-15 21:52:49 +0200

    Removed jQuery dependency

--- lazyYT.js
+++ lazyYT.js
@@ -1,59 +1,67 @@
 /*! LazyYT (lazy load Youtube videos plugin) - v0.3.4 - 2014-06-30
-* Usage: <div class="lazyYT" data-youtube-id="laknj093n" ratio="16:9" data-parameters="rel=0">loading...</div>
+* Usage: <div class='lazyYT' data-youtube-id='laknj093n' ratio='16:9' data-parameters='rel=0'>loading...</div>
 * Copyright (c) 2014 Tyler Pearson; Licensed MIT */
 
+'use strict';
 
-;(function ($) {
-    'use strict';
+var LazyYT = (function () {
+  function setUp(el) {
+    var width = el.dataset.width,
+        height = el.dataset.height,
+        ratio = el.dataset.ratio,
+        id = el.dataset.youtubeId,
+        aspectRatio = ['16', '9'],
+        paddingTop = 0,
+        youtubeParameters = el.dataset.parameters || '';
 
-    function setUp($el) {
-        var width = $el.data('width'),
-            height = $el.data('height'),
-            ratio = $el.data('ratio'),
-            id = $el.data('youtube-id'),
-            aspectRatio = ['16', '9'],
-            paddingTop = 0,
-            youtubeParameters = $el.data('parameters') || '';
-
-        if (typeof width === 'undefined' || typeof height === 'undefined') {
-          height = 0;
-          width = '100%';
-          aspectRatio = (ratio.split(":")[1] / ratio.split(":")[0]) * 100;
-          paddingTop = aspectRatio + '%';
-        }
-
-        $el.css({
-            'position': 'relative',
-            'height': height,
-            'width': width,
-            'padding-top': paddingTop,
-            'background': 'url(http://img.youtube.com/vi/' + id + '/hqdefault.jpg) center center no-repeat',
-            'cursor': 'pointer',
-            'background-size': 'cover'
-        })
-            .html('<p id="lazyYT-title-' + id + '" class="lazyYT-title"></p><div class="lazyYT-button"></div>')
-            .addClass('lazyYT-image-loaded');
-
-        $.getJSON('https://gdata.youtube.com/feeds/api/videos/' + id + '?v=2&alt=json', function (data) {
-            $('#lazyYT-title-' + id).text(data.entry.title.$t);
-        });
-
-        $el.on('click', function (e) {
-            e.preventDefault();
-            if (!$el.hasClass('lazyYT-video-loaded') && $el.hasClass('lazyYT-image-loaded')) {
-                $el.html('<iframe width="' + width + '" height="' + height + '" src="//www.youtube.com/embed/' + id + '?autoplay=1&' + youtubeParameters + '" style="position:absolute; top:0; left:0; width:100%; height:100%;" frameborder="0" allowfullscreen></iframe>')
-                    .removeClass('lazyYT-image-loaded')
-                    .addClass('lazyYT-video-loaded');
-            }
-        });
-
+    if (typeof width === 'undefined' || typeof height === 'undefined') {
+      height = 0;
+      width = '100%';
+      aspectRatio = (ratio.split(':')[1] / ratio.split(':')[0]) * 100;
+      paddingTop = aspectRatio + '%';
+    } else {
+      height = height + 'px';
+      width = width + 'px';
     }
 
-    $.fn.lazyYT = function () {
-        return this.each(function () {
-            var $el = $(this).css('cursor', 'pointer');
-            setUp($el);
-        });
-    };
+    el.style.position = 'relative';
+    el.style.height = height;
+    el.style.width = width;
+    el.style.paddingTop = paddingTop;
+    el.style.background = 'url(http://img.youtube.com/vi/' + id + '/hqdefault.jpg) center center no-repeat';
+    el.style.cursor = 'pointer';
+    el.style.backgroundSize = 'cover';
 
-}(jQuery));
+    el.innerHTML = '<p id="lazyYT-title-' + id + '" class="lazyYT-title"></p><div class="lazyYT-button"></div>';
+    el.classList.add('lazyYT-image-loaded');
+
+    var request = new XMLHttpRequest();
+    request.open('GET', 'https://gdata.youtube.com/feeds/api/videos/' + id + '?v=2&alt=json', true);
+    request.onload = function() {
+      if (request.status >= 200 && request.status < 400){
+        var data = JSON.parse(request.responseText);
+        document.getElementById('lazyYT-title-' + id).innerHTML = data.entry.title.$t;
+      }
+    };
+    request.send();
+
+    el.addEventListener('click', function(e) {
+      e.preventDefault();
+      if (!el.classList.contains('lazyYT-video-loaded') && el.classList.contains('lazyYT-image-loaded')) {
+        el.innerHTML = '<iframe width="' + width + '" height="' + height + '" src="http://www.youtube.com/embed/' + id + '?autoplay=1&' + youtubeParameters +  '" style="position:absolute; top:0; left:0; width:100%; height:100%;" frameborder="0" allowfullscreen></iframe>';
+        el.classList.remove('lazyYT-image-loaded');
+        el.classList.add('lazyYT-video-loaded');
+      }
+    }, false);
+  }
+
+  return {
+    init: function(selector) {
+      var els = document.querySelectorAll(selector);
+      [].forEach.call(els, function(el) {
+        el.style.cursor = 'pointer';
+        setUp(el);
+      });
+    }
+  };
+})();
